b75129f330be99c75734f12459279c113b72fd1a
heismrjava <elesmr.java@gmail.com>
2023-10-17 13:51:33 +0100

    Adding cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const path = require("path");
 const app = express();
@@ -11,6 +12,7 @@ app.set("port", process.env.PORT || 5000);
 //Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
 
 //Routes
 app.use("/api/tasks", require("./routes/index"));
