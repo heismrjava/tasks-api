68c68ff5f9844737949fb04c6ab53cf413a7da69
heismrjava <elesmr.java@gmail.com>
2023-10-23 15:23:27 +0100

    User registration

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 // Importar el controlador de tasks
 const tasks = require("../controllers/task.controller");
+const users = require("../controllers/user.controller");
 
 // Agregar el controlador de tasks a la ruta principal
 router.use("/api/tasks", tasks);
+router.use("/api/users", users);
 
 // Exportar el router
-module.exports = router;
+//module.exports = router;
